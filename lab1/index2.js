58a220ff6b4f9ed0d1d233c84fb59132d7bb7ebf
anshtyagi1818 <anshtyagi@example.com>
2026-09-23 11:32:52 +0530

    Organize lab1 exercises

---
+++ lab1/index2.js
@@ -0,0 +1,56 @@
+const ToWords =(digit)=>{
+    switch (digit){
+     case 1:
+            return "One";
+        case 2:
+            return "Two";
+        case 3:
+            return "Three";
+        case 4:
+            return "Four";
+        case 5:
+            return "Five";
+        case 6:
+            return "Six";
+        case 7:
+            return "Seven";
+        case 8:
+            return "Eight";
+        case 9:
+            return "Nine";
+        case 10:
+            return "Ten";
+       
+    }
+    };
+   const toWords =(digit)=>{
+    switch (digit){
+     case 1:
+            return "One";
+        case 2:
+            return "Two";
+        case 3:
+            return "Three";
+        case 4:
+            return "Four";
+        case 5:
+            return "Five";
+        case 6:
+            return "Six";
+        case 7:
+            return "Seven";
+        case 8:
+            return "Eight";
+        case 9:
+            return "Nine";
+        case 10:
+            return "Ten";
+       
+    }};
+   
+
+    
+
+    console.log(ToWords(5)); 
+console.log(ToWords(10)); 
+console.log(ToWords(15));
